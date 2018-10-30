846b3106a9d69d7acc044c839149db56b6e34760
Rudy Crespo <crespor@UADV-AC-306G3QP.admsu.montclair.edu>
2018-10-30 11:11:12 -0400

    no container around wp_nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,7 @@
     <a class="c-header__logo" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     <nav class="c-header__nav">
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav__list']) !!}
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav__list', 'container' => '']) !!}
       @endif
     </nav>
     <div class="c-header__misc"></div>
