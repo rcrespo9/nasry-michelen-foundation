f455f730b1c39ff36c5b0b619fa6530f11317bca
Rudy Crespo <crespor@UADV-AC-306G3QP.admsu.montclair.edu>
2018-10-30 11:04:04 -0400

    added modular scale and normalize

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,8 +3,9 @@
     <a class="c-header__logo" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     <nav class="c-header__nav">
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'c-header__nav-list']) !!}
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav__list']) !!}
       @endif
     </nav>
+    <div class="c-header__misc"></div>
   </div>
 </header>
