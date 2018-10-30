3b59641cd4ece9a5405d78da8a83503c0c127602
Rudy Crespo <crespor@UADV-AC-306G3QP.admsu.montclair.edu>
2018-10-30 11:45:05 -0400

    starting on nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,13 @@
 <header class="c-header">
-  <div class="l-wrap">
+  <div class="c-header__wrap l-wrap">
     <a class="c-header__logo" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     <nav class="c-header__nav">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav__list', 'container' => '']) !!}
       @endif
     </nav>
-    <div class="c-header__misc"></div>
+    <div class="c-header__misc">
+      <span>EN | ES</span>
+    </div>
   </div>
 </header>
