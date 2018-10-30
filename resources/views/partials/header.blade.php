9a4f765899f1997708ec173123c373f70ecb450a
Rudy Crespo <crespor@UADV-AC-306G3QP.admsu.montclair.edu>
2018-10-30 10:32:23 -0400

    began base styles, add typekit

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,9 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
+<header class="c-header">
+  <div class="l-wrap">
+    <a class="c-header__logo" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+    <nav class="c-header__nav">
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'c-header__nav-list']) !!}
       @endif
     </nav>
   </div>
