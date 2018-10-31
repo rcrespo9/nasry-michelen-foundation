859e15813abee33fbd6ad792b9ca069ded3e0575
Rudy Crespo <crespor@UADV-AC-306G3QP.admsu.montclair.edu>
2018-10-31 14:50:16 -0400

    homepage header, plugged in acf fields

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,7 +1,22 @@
-<div class="{{is_home() || is_front_page() ? 'c-page-header--home' : 'c-page-header--single'}}" style="background-image: {{ get_the_post_thumbnail_url() }};">
+@php
+  $isHome = is_home() || is_front_page();
+  $featuredImage = get_the_post_thumbnail_url();  
+  $blurb = get_field('page_header_blurb');
+  $buttonURL = get_field('page_header_button_url');
+  $buttonText = get_field('page_header_button_text');
+@endphp
+
+<div class="{{$isHome ? 'c-page-header--home' : 'c-page-header--single'}}" style="{{ $featuredImage ? 'background-image:url(' . $featuredImage .');' : '' }}">
   <div class="c-page-header__wrap l-wrap">
     <div class="c-page-header__inner">
       <h1 class="c-page-header__title">{!! App::title() !!}</h1>
+      @if ($blurb)
+        <p class="c-page-header__blurb">{{$blurb}}</p>
+      @endif
+      
+      @if ($isHome)
+        <a class="o-btn" href="{{$buttonURL}}">{{$buttonText}}</a>
+      @endif
     </div>
   </div>
 </div>
