fd07ecb3d91e9cbd575b76441898e66c383189b2
rudycrespo <rudy.crespo20@gmail.com>
2018-10-31 23:00:01 -0400

    get wp attachment image

--- resources/views/components/split-content-block.blade.php
+++ resources/views/components/split-content-block.blade.php
@@ -1,12 +1,12 @@
 @php
-  $image = get_field('split_content_block_image');
+  $imageID = get_field('split_content_block_image');
   $content = get_field('split_content_block_content');
   $isReverse = get_field('split_content_block_direction');
 @endphp
 
 <div class="{{$isReverse ? 'c-split-content-block--reverse' : 'c-split-content-block'}}">
   <div class="c-split-content-block__img">
-    <img src="{{$image}}" alt="">
+    @php echo wp_get_attachment_image($imageID, 'full'); @endphp
   </div>
   <div class="c-split-content-block__text">
     @php echo wp_kses_post($content); @endphp
