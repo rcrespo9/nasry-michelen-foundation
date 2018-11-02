997127a6abcab9a5b5fabe17091165ef85014e13
Rudy Crespo <crespor@UADV-AC-306G3QP.admsu.montclair.edu>
2018-11-02 14:24:12 -0400

    donate prompt

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,9 @@
+<div class="c-donate-prompt">
+  <div class="c-donate-prompt__inner l-wrap">
+    <p class="c-donate-prompt__text">Your donation will go towards scholarships.</p>
+    <a href="#" class="o-btn--secondary">Give Now</a>
+  </div>
+</div>
 <footer class="c-footer">
   @php dynamic_sidebar('sidebar-footer') @endphp
 </footer>
