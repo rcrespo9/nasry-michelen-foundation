b50b2364fa2e6d45cc1ab5144fbeb137d9159a82
rudycrespo <rudy.crespo20@gmail.com>
2018-11-04 12:43:26 -0500

    some quick js for mobile nav

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,20 @@
 export default {
+  data: {
+    isNavOpen: false,
+  },
   init() {
     // JavaScript to be fired on all pages
+  },
+  openNav() {
+    const $body = document.body;
+
+    this.data.isNavOpen = !this.data.isNavOpen;
+
+    if (this.data.isNavOpen) {
+      $body.classList.remove('nav-open');
+    } else {
+      $body.classList.add('nav-open');
+    }
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
