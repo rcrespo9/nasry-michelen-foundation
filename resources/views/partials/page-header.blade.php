f0394518968fd86f5b2857ee67f72cdfac33363c
rudycrespo <rudy.crespo20@gmail.com>
2018-11-06 15:22:51 -0500

    page header title

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,6 +1,7 @@
 @php
   $isHome = is_home() || is_front_page();
   $featuredImage = get_the_post_thumbnail_url();  
+  $title = get_field('page_header_title');
   $blurb = get_field('page_header_blurb');
   $buttonURL = get_field('page_header_button_url');
   $buttonText = get_field('page_header_button_text');
@@ -8,7 +9,7 @@
 
 <header class="{{$isHome ? 'c-page-header--home' : 'c-page-header--single'}}" style="{{ $featuredImage ? 'background-image:url(' . $featuredImage .');' : '' }}">
   <div class="c-page-header__wrap l-wrap">
-    <h1 class="c-page-header__title">{!! App::title() !!}</h1>
+    <h1 class="c-page-header__title">{{ $title ? $title : App::title() }}</h1>
     @if ($blurb)
       <p class="c-page-header__blurb">{{$blurb}}</p>
     @endif
