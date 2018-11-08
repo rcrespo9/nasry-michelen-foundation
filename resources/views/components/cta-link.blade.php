ef870e34545934062f226a589d78e870dc9af2dc
rudycrespo <rudy.crespo20@gmail.com>
2018-11-07 23:01:41 -0500

    secondary section styles

--- resources/views/components/cta-link.blade.php
+++ resources/views/components/cta-link.blade.php
@@ -1,4 +1,4 @@
-<a href="{{$url}}" class="o-cta-link {{$class}}">
+<a href="{{$url}}" class="o-cta-link {{$class ? $class : ''}}">
   <span class="o-cta-link__text">{{ $slot }}</span>
   <svg class="o-cta-link__arrow icon icon-arrow-right"><use xlink:href="#icon-arrow-right"></use></svg>
 </a>
--- resources/views/partials/announcements.blade.php
+++ resources/views/partials/announcements.blade.php
@@ -10,9 +10,11 @@
 
 <section class="c-page-section c-page-section--secondary">
   <div class="l-wrap">
-    <div class="c-page-section__header u-text-center">      
+    <div class="c-page-section__header">      
       <h2 class="c-page-section__title">Announcements</h2>
-      {{-- <a href="{{get_permalink(get_post(56)->ID)}}" class="c-page-section__cta">View All</a> --}}
+      @component('components.cta-link', ['url' => get_permalink(get_post(56)->ID), 'class' => 'c-page-section__cta-link'])
+        View more announcements
+      @endcomponent
     </div>
     <div class="l-announcements">
       @if($the_query->have_posts())
