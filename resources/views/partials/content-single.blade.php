ad91c0d7f0699d261afe5962ef297608c11fb700
rudycrespo <rudy.crespo20@gmail.com>
2018-11-09 18:05:47 -0500

    event meta fields

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -2,7 +2,7 @@
   @include('partials.page-header')
   <div class="c-article__content l-wrap--small">
     <div class="content__meta">
-      @include('partials/entry-meta')
+      @include('partials/entry-meta', ['show_event_location' => true])
     </div>
     @php the_content() @endphp
   </div>
--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -1,1 +1,9 @@
-<time class="o-date" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
+@php
+  $event_date_time = get_field('event_date_time');
+  $event_location = get_field('event_location');
+@endphp
+
+<time class="o-meta" datetime="{{ $event_date_time ? date('c', strtotime($event_date_time)) : get_post_time('c', true) }}">{{ $event_date_time ? $event_date_time : get_the_date() }}</time>
+@if ($event_location && $show_event_location)
+<span class="o-meta">at {{$event_location}}</span>
+@endif
