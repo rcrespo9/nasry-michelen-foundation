44225be8e0bd23d78ce723dbbc6084e2fc01050b
rudycrespo <rudy.crespo20@gmail.com>
2018-11-10 14:40:19 -0500

    no padding container

--- resources/views/components/page-section.blade.php
+++ resources/views/components/page-section.blade.php
@@ -1,5 +1,5 @@
 <section class="c-page-section{{$isSecondaryClass ? ' c-page-section--secondary' : ''}} {{is_front_page() ? 'c-page-section--home' : ''}}">
-  <div class="l-wrap">
+  <div class="{{$noPadding ? 'l-wrap--no-padding' : 'l-wrap'}}">
     @if ($header)
       <div class="c-page-section__header">
         <h2 class="c-page-section__title">{{$header}}</h2>
--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -4,13 +4,13 @@
   @include('partials.homepage-header')
   <div class="c-homepage-content">  
     @if ( have_rows('split_content_block') )
-      @component('components.page-section')
+      @component('components.page-section', ['noPadding' => true])
         @include('partials.split-content-block', ['blockIndex' => 0])
       @endcomponent
     @endif
     @include('partials.recent-announcements')
     @if ( have_rows('split_content_block') && count(get_field('split_content_block')) > 1 )
-      @component('components.page-section')
+      @component('components.page-section', ['noPadding' => true])
         @include('partials.split-content-block', ['blockIndex' => 1])
       @endcomponent
     @endif
