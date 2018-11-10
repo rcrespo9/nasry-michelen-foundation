370247d5961cbc90ba359088351d2815c02bddf8
rudycrespo <rudy.crespo20@gmail.com>
2018-11-10 14:06:41 -0500

    starting media queries

--- resources/views/partials/split-content-block.blade.php
+++ resources/views/partials/split-content-block.blade.php
@@ -8,9 +8,7 @@
 @endphp
 
 <div class="{{$isReverse ? 'c-split-content-block--reverse' : 'c-split-content-block'}}">
-  <div class="c-split-content-block__img">
-    @php echo wp_get_attachment_image($imageID, 'full'); @endphp
-  </div>
+  <div class="c-split-content-block__img" style="background-image: url({{wp_get_attachment_url($imageID)}});"></div>
   <div class="c-split-content-block__text">
     @php echo wp_kses_post($content); @endphp
     @if ($ctaText)
