c45ff37f105eebc34ae7e3ee26a0ba6625594b83
rudycrespo <rudy.crespo20@gmail.com>
2018-11-10 18:42:32 -0500

    mobile nav toggle

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,20 +1,33 @@
 export default {
   data: {
     isNavOpen: false,
+    theNav: document.querySelector('.js-nav'),
+    theMobileNavBtn: document.querySelector('.js-nav-trigger'),
+    theBody: document.body,
+    noScrollClass: 'no-scroll',
+    navOpenClass: 'js-nav--open',
   },
   init() {
-    // JavaScript to be fired on all pages
+    const isMinTabletWidth = window.matchMedia(`(min-width: 930px)`);
+    const checkIfTablet = (e) => e.matches ? this.destroyMobileNav() : this.addMobileNavBtnListener();
+
+    checkIfTablet(isMinTabletWidth); // check media query on init
+    isMinTabletWidth.addListener(checkIfTablet); // add media query listener
   },
   openNav() {
-    const $body = document.body;
-
-    this.data.isNavOpen = !this.data.isNavOpen;
-
-    if (this.data.isNavOpen) {
-      $body.classList.remove('nav-open');
-    } else {
-      $body.classList.add('nav-open');
-    }
+    this.data.theBody.classList.add(this.data.noScrollClass);
+    this.data.theNav.classList.add(this.data.navOpenClass);
+  },
+  closeNav() {
+    this.data.theBody.classList.remove(this.data.noScrollClass);
+    this.data.theNav.classList.remove(this.data.navOpenClass);
+  },
+  addMobileNavBtnListener() {
+    this.data.theMobileNavBtn.addEventListener('click', () => this.openNav(), false);
+  },
+  destroyMobileNav() {
+    this.closeNav();
+    this.data.theMobileNavBtn.removeEventListener('click', this.openNav, false);
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header class="c-header">
   <div class="c-header__inner l-wrap">
     <a class="c-header__logo" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="c-header__nav">
+    <nav class="c-header__nav js-nav">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav__list', 'container' => '']) !!}
       @endif
@@ -9,5 +9,8 @@
         <a href="{{get_permalink(get_post(127)->ID)}}" class="o-btn--primary">Donate</a>
       </div>
     </nav>
+    <div class="c-header__nav-trigger">
+      <button class="nav-trigger__btn js-nav-trigger">Menu</button>
+    </div>
   </div>
 </header>
