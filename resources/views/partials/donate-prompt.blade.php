9c7226f562e4cb6b9ad9e9b7861ca2f0d68a1497
rudycrespo <rudy.crespo20@gmail.com>
2018-11-11 18:19:05 -0500

    mostly responsive footer

--- resources/views/partials/donate-prompt.blade.php
+++ resources/views/partials/donate-prompt.blade.php
@@ -1,6 +1,6 @@
 <div class="c-donate-prompt">
   <div class="c-donate-prompt__inner l-wrap">
     <p class="c-donate-prompt__text">Your donation will go towards scholarships.</p>
-    <a href="{{get_permalink(get_post(127)->ID)}}" class="o-btn--secondary">Give Now</a>
+    <a href="{{get_permalink(get_post(127)->ID)}}" class="cta-donate-prompt__btn o-btn--secondary">Give Now</a>
   </div>
 </div>
