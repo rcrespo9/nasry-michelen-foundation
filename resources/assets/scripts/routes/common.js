2f94789b35b5d7c0b7bbc6af61224a79ee73838b
rudycrespo <rudy.crespo20@gmail.com>
2018-11-10 23:50:06 -0500

    mobile menu mostly done

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,8 +1,12 @@
+import $ from 'jquery';
+
 export default {
   data: {
     isNavOpen: false,
     theNav: document.querySelector('.js-nav'),
     theMobileNavBtn: document.querySelector('.js-nav-trigger'),
+    theMobileNavList: document.querySelector('.js-nav-list'),
+    allSubMenus: document.querySelectorAll('.menu-item-has-children .sub-menu'),
     closeMobileNavBtn: document.querySelector('.js-nav-close'),
     theBody: document.body,
     noScrollClass: 'no-scroll',
@@ -23,14 +27,26 @@ export default {
     this.data.theBody.classList.remove(this.data.noScrollClass);
     this.data.theNav.classList.remove(this.data.navOpenClass);
   },
+  toggleNavDropdown(e) {
+    const isDropdownListItem = e.target && e.target.nodeName === 'LI' && e.target.classList.contains('menu-item-has-children');
+
+    if (isDropdownListItem) {
+      const $subMenu = $(e.target.lastElementChild);
+
+      $subMenu.slideToggle();
+    }
+  },
   addNavListeners() {
     this.data.theMobileNavBtn.addEventListener('click', () => this.openNav(), false);
     this.data.closeMobileNavBtn.addEventListener('click', () => this.closeNav(), false);
+    this.data.theMobileNavList.addEventListener('click', this.toggleNavDropdown, false);
   },
   destroyMobileNav() {
     this.closeNav();
     this.data.theMobileNavBtn.removeEventListener('click', this.openNav, false);
     this.data.closeMobileNavBtn.removeEventListener('click', () => this.closeNav, false);
+    this.data.theMobileNavList.removeEventListener('click', this.toggleNavDropdown, false);
+    this.data.allSubMenus.forEach(el => el.style = '');
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,7 @@
     <a class="c-header__logo" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     <nav class="c-header__nav js-nav">
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav__list', 'container' => '']) !!}
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav__list js-nav-list', 'container' => '']) !!}
       @endif
       <div class="nav__donate">
         <a href="{{get_permalink(get_post(127)->ID)}}" class="o-btn--primary">Donate</a>
