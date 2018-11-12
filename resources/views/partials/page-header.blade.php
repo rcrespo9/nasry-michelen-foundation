b2cf8108099e92b9435c2548cb5a9526e6019375
rudycrespo <rudy.crespo20@gmail.com>
2018-11-12 18:52:09 -0500

    removed category from category page title

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,6 +1,7 @@
 @php
   $featuredImage = get_the_post_thumbnail_url();
   $parent = get_the_title($post->post_parent);
+  $category = get_queried_object();
 @endphp
 
 <header class="c-page-header {{ $featuredImage ? 'has-bg-img' : '' }}" style="{{ $featuredImage ? 'background-image:url(' . $featuredImage .');' : '' }}">
@@ -15,7 +16,7 @@
           @endif
         </div>
       @endif
-      <h1 class="c-page-header__title">{{ App::title() }}</h1>
+      <h1 class="c-page-header__title">{{ is_category() ? get_cat_name($category->term_id) : App::title() }}</h1>
       <div class="c-page-header__breadcrumbs">{{ App::breadcrumbs() }}</div>
     </div>
   </div>
