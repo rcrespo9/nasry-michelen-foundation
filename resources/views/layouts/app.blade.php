2b5344e902f554a07463cdace5304a204ce7ed56
rudycrespo <rudy.crespo20@gmail.com>
2018-11-11 21:28:19 -0500

    added site content wrapper

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,12 @@
     @include('partials.svg-symbols')
     @php do_action('get_header') @endphp
     @include('partials.header')
-    <main class="c-site-container">
-      @yield('content')
-      @include('partials.donate-prompt')
-    </main>
+    <div class="c-site-container">
+      <main class="c-site-content">
+        @yield('content')
+        @include('partials.donate-prompt')
+      </main>
+    </div>
     @php do_action('get_footer') @endphp
     @include('partials.footer')
     @php wp_footer() @endphp
